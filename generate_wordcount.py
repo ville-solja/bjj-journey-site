dec96f77c20b2fead35a2f11a74345557649a5e9
ville-solja <ville.solja@gmail.com>
2025-08-08 10:58:17 +0300

    Update generate_wordcount.py

--- generate_wordcount.py
+++ generate_wordcount.py
@@ -37,4 +37,4 @@ if __name__ == "__main__":
     with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
         f.write(content)
 
-    print(f"Word count written to: {OUTPUT_FILE} and vault_wordcount.txt")
+    print(f"Word count written to: {OUTPUT_FILE}")
