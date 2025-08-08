8dbe19c2ff65d9d973f59c274ec165c5c790918c
ville-solja <ville.solja@gmail.com>
2025-08-08 10:18:31 +0300

    Update VaultStats.tsx

--- quartz/components/VaultStats.tsx
+++ quartz/components/VaultStats.tsx
@@ -1,3 +1,11 @@
+import { QuartzComponentConstructor } from "./types"
+
 const VaultStats: QuartzComponentConstructor = () => {
-  return () => <div>✅ VaultStats working!</div>
+  return () => (
+    <div style={{ marginTop: "2rem", fontSize: "0.9rem", lineHeight: "1.4" }}>
+      VaultStats component is working!
+    </div>
+  )
 }
+
+expor
